cf5f5fb42bcd4bbfe1a716f0c082967b194877de
saul1917 <saul1917@gmail.com>
2012-07-16 02:41:55 +0000

    Primer version. Funcionalidades: 1.Activa alarma cuando detecta movimiento 2.Se arma y desarma y se desactiva con codigo.

---
+++ Alarma/src/alarma/Controlador.java
@@ -0,0 +1,109 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alarma;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+/**
+ *
+ * @author Saul Calderon
+ */
+public class Controlador {
+    /** El comunicador arduino. */
+    private ComunicadorArduino comunicadorArduino;
+    
+    private boolean alarmaActivada = false;
+    
+    /** Ventana principal*/
+    private VentanaPrincipal ventana;
+    
+    public Controlador(){
+        this.comunicadorArduino = new ComunicadorArduino();
+        
+    }
+    
+    public void iniciar(VentanaPrincipal ventana){
+        this.ventana = ventana;
+        this.comunicadorArduino.iniciar(9600, this);    
+        this.comunicadorArduino.setLeerArduino(false);
+        
+    }
+    
+    public void agregarABitacora(String entrada){
+        if(entrada.compareTo("---") == 0){
+            this.activarAlarma();
+        }
+        this.ventana.agregarABitacora(entrada);
+    }
+    
+    public void conmutarAlarma(){
+       if(!alarmaActivada){
+           
+            boolean armada = this.comunicadorArduino.leyendoArduino();
+            //Esta armada
+            if(armada){//Desarmar la alarma
+                String password = this.ventana.getCampoPassword();
+                if(password.compareTo("123")== 0){
+                    this.ventana.cambiarEtiquetaAlarma("Armar alarma");                    
+                    this.ventana.mostrarMensaje("Contraseña correcta, alarma desarmada");
+                    this.comunicadorArduino.setLeerArduino(false);
+                }
+                else{
+                    this.ventana.mostrarMensaje("Codigo incorrecto");
+                }
+            }
+            else{//si no estaba armada, arma la alarma
+                this.ventana.cambiarEtiquetaAlarma("Desarmar alarma");
+                this.ventana. setMensajeBarraEstado("Alarma armada");
+                this.comunicadorArduino.setLeerArduino(true);
+            }
+       }
+       else{
+           String password = this.ventana.getCampoPassword();
+           if(password.compareTo("123")== 0){
+               this.alarmaActivada = false;
+               this.ventana.cambiarEtiquetaAlarma("Armar alarma");               
+               this.ventana.mostrarMensaje("Contraseña correcta, alarma desactivada");
+               this.comunicadorArduino.setLeerArduino(false);
+           }
+           else{
+                this.ventana.mostrarMensaje("Contraseña incorrecta");
+           }
+       
+       }
+           
+    }
+    
+    public static synchronized void playSound(final String url) {
+        new Thread(new Runnable() { // the wrapper thread is unnecessary, unless it blocks on the Clip finishing, see comments
+        public void run() {
+            try {
+                Clip clip = AudioSystem.getClip();
+                AudioInputStream inputStream = AudioSystem.getAudioInputStream(VentanaPrincipal.class.getResourceAsStream("" + url));
+                clip.open(inputStream);
+                
+                clip.start(); 
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        }).start();
+    }
+
+    
+    public void activarAlarma(){
+        boolean armada = this.comunicadorArduino.leyendoArduino();
+        if(armada){
+            this.alarmaActivada = true;
+            this.ventana.cambiarEtiquetaAlarma("Desactivar Alarma");
+            playSound("warning.wav");
+            this.ventana.setMensajeBarraEstado("Alarma activada!!");
+           // this.ventana.mostrarMensaje("Alarma activada!!!");
+        }
+    }
+    
+}
